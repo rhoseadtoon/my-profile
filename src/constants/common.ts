7561c50b637df6820a144f4f93c3133a88cddcb3
nestle21 <adtoonrhose@gmail.com>
2024-09-09 08:43:32 +0800

    main

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Rhose Dinmar Adto-on';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+'A dedicated 4th-year Information Technology student with hands-on experience in writing, testing, debugging, and maintaining software code to develop functional applications and systems. Proficient in programming languages, algorithms, data structures, and familiar with software development methodologies, eager to apply academic knowledge in real-world settings and contribute to innovative projects.';
+export const ABOUT_TEXT_2 =
+  'Responsible for developing and maintaining web applications, handling both frontend and backend tasks. Collaborated with cross-functional teams to design, implement, and deploy scalable solutions aligned with business goals.';
+export const ABOUT_TEXT_3 =
+  'Motivated 4th-year student proficient in both frontend and backend development, capable of building web applications from start to finish. Possesses a solid understanding of programming languages, frameworks, and databases, with a keen interest in learning and applying new technologies.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Aspiring Software Developer with experience in building websites and web applications using modern JavaScript tools and frameworks. Possesses foundational knowledge of front-end and back-end technologies, as well as database management.';
+export const RESUME_TEXT_2 =
+  '4th-year Information Technology student specializing in building websites and web applications using modern JavaScript tools and frameworks.';
